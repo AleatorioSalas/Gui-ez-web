3dcd8a67fc093d5076f93bff3ddd93188a5e6759
Mariano Lukas Salas Diaz <msalas@guinezingenieria.cl>
2024-10-30 09:26:06 -0300

    Descripción breve de los cambios

--- crud/routes/web.php
+++ crud/routes/web.php
@@ -13,3 +13,6 @@ Route::post("/modificar-usuario", [CrudController::class, "update"])->name("crud
 
 //Ruta para eliminar un usuario
 Route::get("/eliminar-usuario--{id}", [CrudController::class, "delete"])->name("crud.delete");
+
+//ruta para asociar la tabla llamada bam
+Route::get('/bam', [bamController::class, 'index'])->name('bam.index');
